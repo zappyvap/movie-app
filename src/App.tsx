358663276452f95f45bef0989140993034d5fb5f
Peter Hart <peterjhart1010@gmail.com>
2025-09-20 11:13:29 -0400

    added comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { useState } from 'react';
+import { useState, type SetStateAction } from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar';
 import Movie from './components/Movie';
 
-
-
-interface MovieData {
+interface MovieData { // interface that describes movie data
   id: number;
   title: string;
   poster_path: string;
@@ -15,15 +13,15 @@ interface MovieData {
 }
 
 const App = () => {
-  const [query, setQuery] = useState('');
-  const [movies, setMovies] = useState<MovieData[]>([]);
+  const [query, setQuery] = useState(''); // varibles to set the searchbar
+  const [movies, setMovies] = useState<MovieData[]>([]); // setting the movie array variables
   const [selectedMovie, setSelectedMovie] = useState(null);
 
-  const handleMovieSelect = (movie) => {
+  const handleMovieSelect = (movie) => { // uses function to set the selected movie
     setSelectedMovie(movie);
   };
 
-  const handleBackToSearch = () => {
+  const handleBackToSearch = () => { // function for back to search button that puts the selected movie to null
     setSelectedMovie(null);
   };
 
@@ -41,9 +39,21 @@ const App = () => {
   return (
     <>
       <h1>Movie Catalog</h1>
-      {selectedMovie ? (
+      {selectedMovie ? ( // shows this if someone selects a movie
         <MovieDetail movie={selectedMovie} />
-      ) : (
+      ) : ( // shows this otherwise
+        /**
+         * 
+         * What this block of code does below:
+         * 
+         * Makes a searchbar component and passes in the query and the functions
+         * defined for it
+         * 
+         * Makes a div that holds the first 3 results of the movie array and shows
+         * the poster and the title of the movie. It also has an onClick function that
+         * will change the selectedMovie variable making this code go away and show the code
+         * that is the true path of the ternary expression.
+        */
         <>
           <SearchBar query={query} setQuery={setQuery} setMovies={setMovies} />
           <div className="movie-list-container">
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,6 @@
 import './SearchBar.css';
 
-interface Props {
+interface Props { // props that are inputted from app.tsx
   query: string;
   setQuery: (value: string) => void;
   setMovies: (movies: []) => void;
@@ -10,9 +10,9 @@ const SearchBar: React.FC<Props> = ({ query, setQuery, setMovies }) => {
 
   const onClick = () => {
     const url = `https://api.themoviedb.org/3/search/movie?query=${query
-      .split(" ")
+      .split(" ") // splits the movie name and adds %20 in between words to make the URL complete
       .join("%20")}&include_adult=true&language=en-US&page=1`;
-    const options = {
+    const options = { // this just uses an authorization key so I can fetch the data
       method: "GET",
       headers: {
         accept: "application/json",
@@ -21,8 +21,8 @@ const SearchBar: React.FC<Props> = ({ query, setQuery, setMovies }) => {
       },
     };
     fetch(url, options)
-      .then((res) => res.json())
-      .then((json) => {
+      .then((res) => res.json()) // gets the json
+      .then((json) => { // takes the results of the json and adds it to the movie list
         if (json.results.length === 0) {
           console.log("No Results Found, Please Try Again.");
           setMovies([]);
@@ -33,9 +33,9 @@ const SearchBar: React.FC<Props> = ({ query, setQuery, setMovies }) => {
       .catch((err) => console.error(err));
   };
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setQuery(e.target.value);
+    setQuery(e.target.value); // function that changes the textbox text
   };
-  return (
+  return ( // basic textbox setup
     <div>
       <input
         type="text"
